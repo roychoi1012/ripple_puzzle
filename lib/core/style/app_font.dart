b20072294cbd94b4c54d69e141fc7c26b38ca001
roychoi <roychoi1012@naver.com>
2025-06-13 15:10:59 +0900

    feat: Create app font

---
+++ lib/core/style/app_font.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class AppFonts {
+  // Bold TextStyles (Orbitron)
+  static final TextStyle titleTextBold = GoogleFonts.orbitron(
+    fontSize: 48,
+    height: 75 / 50,
+    fontWeight: FontWeight.bold,
+  );
+  
+  static final TextStyle headerTextBold = GoogleFonts.orbitron(
+    fontSize: 40,
+    height: 45 / 30,
+    fontWeight: FontWeight.bold,
+  );
+  
+  static final TextStyle largeTextBold = GoogleFonts.orbitron(
+    fontSize: 24,
+    height: 30 / 20,
+    fontWeight: FontWeight.bold,
+  );
+  
+  static final TextStyle mediumTextBold = GoogleFonts.orbitron(
+    fontSize: 20,
+    height: 27 / 18,
+    fontWeight: FontWeight.bold,
+  );
+  
+  static final TextStyle normalTextBold = GoogleFonts.orbitron(
+    fontSize: 18,
+    height: 24 / 16,
+    fontWeight: FontWeight.bold,
+  );
+  
+  static final TextStyle smallTextBold = GoogleFonts.orbitron(
+    fontSize: 14,
+    height: 21 / 14,
+    fontWeight: FontWeight.bold,
+  );
+  
+  static final TextStyle smallerTextBold = GoogleFonts.orbitron(
+    fontSize: 12,
+    height: 17 / 11,
+    fontWeight: FontWeight.bold,
+  );
+
+  // Regular TextStyles (Orbitron)
+  static final TextStyle titleTextRegular = GoogleFonts.orbitron(
+    fontSize: 48,
+    height: 75 / 50,
+    fontWeight: FontWeight.normal,
+  );
+  
+  static final TextStyle headerTextRegular = GoogleFonts.orbitron(
+    fontSize: 40,
+    height: 45 / 30,
+    fontWeight: FontWeight.normal,
+  );
+  
+  static final TextStyle largeTextRegular = GoogleFonts.orbitron(
+    fontSize: 24,
+    height: 30 / 20,
+    fontWeight: FontWeight.normal,
+  );
+  
+  static final TextStyle mediumTextRegular = GoogleFonts.orbitron(
+    fontSize: 20,
+    height: 27 / 18,
+    fontWeight: FontWeight.normal,
+  );
+  
+  static final TextStyle normalTextRegular = GoogleFonts.orbitron(
+    fontSize: 18,
+    height: 24 / 16,
+    fontWeight: FontWeight.normal,
+  );
+  
+  static final TextStyle smallTextRegular = GoogleFonts.orbitron(
+    fontSize: 14,
+    height: 21 / 14,
+    fontWeight: FontWeight.normal,
+  );
+  
+  static final TextStyle smallerTextRegular = GoogleFonts.orbitron(
+    fontSize: 12,
+    height: 17 / 11,
+    fontWeight: FontWeight.normal,
+  );
+
+  // Medium TextStyles (Orbitron) - 추가
+  static final TextStyle titleTextMedium = GoogleFonts.orbitron(
+    fontSize: 48,
+    height: 75 / 50,
+    fontWeight: FontWeight.w500,
+  );
+  
+  static final TextStyle headerTextMedium = GoogleFonts.orbitron(
+    fontSize: 40,
+    height: 45 / 30,
+    fontWeight: FontWeight.w500,
+  );
+  
+  static final TextStyle largeTextMedium = GoogleFonts.orbitron(
+    fontSize: 24,
+    height: 30 / 20,
+    fontWeight: FontWeight.w500,
+  );
+  
+  static final TextStyle mediumTextMedium = GoogleFonts.orbitron(
+    fontSize: 20,
+    height: 27 / 18,
+    fontWeight: FontWeight.w500,
+  );
+  
+  static final TextStyle normalTextMedium = GoogleFonts.orbitron(
+    fontSize: 18,
+    height: 24 / 16,
+    fontWeight: FontWeight.w500,
+  );
+  
+  static final TextStyle smallTextMedium = GoogleFonts.orbitron(
+    fontSize: 14,
+    height: 21 / 14,
+    fontWeight: FontWeight.w500,
+  );
+  
+  static final TextStyle smallerTextMedium = GoogleFonts.orbitron(
+    fontSize: 12,
+    height: 17 / 11,
+    fontWeight: FontWeight.w500,
+  );
+
+  // Light TextStyles (Orbitron) - 추가
+  static final TextStyle titleTextLight = GoogleFonts.orbitron(
+    fontSize: 48,
+    height: 75 / 50,
+    fontWeight: FontWeight.w300,
+  );
+  
+  static final TextStyle headerTextLight = GoogleFonts.orbitron(
+    fontSize: 40,
+    height: 45 / 30,
+    fontWeight: FontWeight.w300,
+  );
+  
+  static final TextStyle largeTextLight = GoogleFonts.orbitron(
+    fontSize: 24,
+    height: 30 / 20,
+    fontWeight: FontWeight.w300,
+  );
+  
+  static final TextStyle mediumTextLight = GoogleFonts.orbitron(
+    fontSize: 20,
+    height: 27 / 18,
+    fontWeight: FontWeight.w300,
+  );
+  
+  static final TextStyle normalTextLight = GoogleFonts.orbitron(
+    fontSize: 18,
+    height: 24 / 16,
+    fontWeight: FontWeight.w300,
+  );
+  
+  static final TextStyle smallTextLight = GoogleFonts.orbitron(
+    fontSize: 14,
+    height: 21 / 14,
+    fontWeight: FontWeight.w300,
+  );
+  
+  static final TextStyle smallerTextLight = GoogleFonts.orbitron(
+    fontSize: 12,
+    height: 17 / 11,
+    fontWeight: FontWeight.w300,
+  );
+}
